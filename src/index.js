377a1baa8dffb6af10892c68a0c12b3c2bea77f2
rossbrandon <wbrandon@adobe.com>
2019-01-09 16:33:28 -0600

    Added Prisma db support

--- src/index.js
+++ src/index.js
@@ -1,53 +1,25 @@
-const {
-    GraphQLServer
-} = require('graphql-yoga')
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const Subscription = require('./resolvers/Subscription')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => links.find(item => item.id == args.id)
-    },
-
-    Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
-        },
-
-        updateLink: (parent, args) => {
-            const link = links.find(item => item.id == args.id)
-            link.description = args.description ? args.description : link.description
-            link.url = args.url ? args.url : link.url
-            return link
-        },
-
-        deleteLink: (parent, args) => {
-            const link = links.find(item => item.id == args.id)
-            if (link) {
-                links.pop(link)
-                return link
-            }
-        }
-    }
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link
 }
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: request => {
+    return { ...request, prisma }
+  }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
