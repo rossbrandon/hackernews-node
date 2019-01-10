cc649d10822f2ebc48844c705a7cd07844f14d3d
rosstafarian <rosstafarian1@gmail.com>
2019-01-09 20:28:45 -0600

    Added filtering, pagination, sorting, and vote count

--- src/resolvers/Link.js
+++ src/resolvers/Link.js
@@ -2,6 +2,17 @@ function postedBy(parent, args, context) {
   return context.prisma.link({ id: parent.id }).postedBy()
 }
 
+function votes(parent, args, context) {
+  return context.prisma.link({ id: parent.id }).votes()
+}
+
+async function voteCount(parent, args, context) {
+  const votes = await context.prisma.link({ id: parent.id }).votes()
+  return votes.length
+}
+
 module.exports = {
-  postedBy
+  postedBy,
+  votes,
+  voteCount
 }
